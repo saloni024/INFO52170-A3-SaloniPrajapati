72edc2e918d056f098b2d4450a4521146f8e2644
Saloni Prajapati <saloniprajapati024@gmail.com>
2026-07-09 14:04:03 -0400

    Add GitHub Actions workflow debugged - Saloni Prajapati

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py - Created by Saloni Prajapati
 def greet(name):
-return f"Hello, {name}! Welcome to INFO 52170."
+    return f"Hello, {name}! Welcome to INFO 52170."
 if __name__ == "__main__":
-print(greet("Saloni"))
+    print(greet("Saloni"))
